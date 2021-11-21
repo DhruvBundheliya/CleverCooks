cd7f539be3047e701e014c661b659344d0061b41
Dark Coder <85485535+DhruvBundheliya@users.noreply.github.com>
2021-11-21 12:19:49 +0530

    first commit

--- Recipe/urls.py
+++ Recipe/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path,include
 from Recipe import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('',views.index,name='index'),
     path('postrecipe',views.postrecipe,name='postrecipe'),
     path('recipes',views.recipes,name='recipes'),
     path('recipe_desc',views.recipe_desc,name='recipe_desc')
-]
+]  + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- User/urls.py
+++ User/urls.py
@@ -1,5 +1,7 @@
 from User import views
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('register',views.register,name='register'),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('profile',views.profile,name='profile'),
     path('logout',views.logoutUser,name='logout'),
     path('account-verify/<slug:token>',views.account_verify,name='account_verify')
-]
+]  + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
