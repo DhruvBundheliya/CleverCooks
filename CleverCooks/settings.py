778d5912e2e52a8a739b175e348db1404c486fc6
Dark Coder <85485535+DhruvBundheliya@users.noreply.github.com>
2021-11-25 15:43:47 +0530

    First Commit

--- CleverCooks/settings.py
+++ CleverCooks/settings.py
@@ -143,5 +143,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dvbundheliya05@gmail.com'
-EMAIL_HOST_PASSWORD = 'fwivxjxwvsnldbua'
+EMAIL_HOST_USER = 'milanpolara2886@gmail.com'
+EMAIL_HOST_PASSWORD = 'wvsvjbvarozbaxbm'
